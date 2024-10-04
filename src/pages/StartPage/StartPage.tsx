c55a2d45d9677ab5830cbf537a4cd16c357bdd05
Ramazan Azimli <80627823+antisyst@users.noreply.github.com>
2024-10-04 19:51:20 +0400

    Update StartPage.tsx

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import MainLogo from '../../assets/logo.svg';
-import { AppRoot, Button, FixedLayout, Spinner, Text } from '@telegram-apps/telegram-ui';
+import { AppRoot, Button, FixedLayout, Spinner, Text, Avatar } from '@telegram-apps/telegram-ui';
 import { useHapticFeedback } from '@telegram-apps/sdk-react';
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
 import './StartPage.scss';
@@ -45,11 +45,15 @@ const StartPage: React.FC = () => {
         <div className="user-info">
           {userInfo && (
             <div className="user-details">
+              {userInfo.photo_url && <Avatar src={userInfo.photo_url} size={96} className="user-avatar" />}
+              <Text>User ID: {userInfo.id}</Text>
               <Text>First Name: {userInfo.first_name}</Text>
               <Text>Last Name: {userInfo.last_name}</Text>
               <Text>Username: {userInfo.username}</Text>
               <Text>Language Code: {userInfo.language_code}</Text>
-              {userInfo.is_premium && <Text>Premium User</Text>}
+              <Text>Premium User: {userInfo.is_premium ? 'Yes' : 'No'}</Text>
+              <Text>Bot: {userInfo.is_bot ? 'Yes' : 'No'}</Text>
+              <Text>Allows Messages: {userInfo.allows_write_to_pm ? 'Yes' : 'No'}</Text>
             </div>
           )}
         </div>
